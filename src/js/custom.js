d80678274199919955efb05f1459c0f023fa6094
Can Aydin <caydinc@gmail.com>
2016-03-27 23:48:51 +0000

    and get the login working with ajax

--- src/js/custom.js
+++ src/js/custom.js
@@ -15,19 +15,46 @@ $(function() {
         $(".navbar-collapse").collapse('hide');
     });
 
+    $('#login-form').submit(function(event) {
+      event.preventDefault();
+      event.preventDefault();
+      var $inputs = $('#login-form :input');
+      var values = {};
+      $inputs.each(function() {
+              values[this.name] = $(this).val();
+      });
+
+      var authfield = btoa(values['username']+":"+values['password']);
+      var data = $(this).serializeArray();
+
+      $.ajax({
+        type : "GET",
+        url: "/api/accounts/" + values['username'],
+        data: data,
+        xhrFields: { withCredentials: true },
+        headers: { "Authorization": 'Basic ' + authfield }
+      }).done(function( data ) {$('#login').modal('hide');}); 
+    });
     $('#register-form').submit(function(event) {
       event.preventDefault();
+      var $inputs = $('#register-form :input');
+      var values = {};
+      $inputs.each(function() {
+	      values[this.name] = $(this).val();
+      });
+
+      var authfield = btoa(values['username']+":"+values['password']);
       var data = $(this).serializeArray();
+
       $.ajax({
         type : "POST",
         url: "/api/accounts",
         data: data,
-        headers: {
-          "Authorization": "Basic foobar"
-        }
+        xhrFields: { withCredentials: true },
+	headers: { "Authorization": 'Basic ' + authfield }
       })
       .done(function( data ) {
-          $( '#register-form' ).empty();
+          $( '#register-form' ).html("Great! Now you can login and submit your IP to the whitelist!");
         });
     });
 });
