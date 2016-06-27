da9a59a4e584b7c2d1b6a453303ea0c9d38c1674
badgamernl <badgamernl@gmail.com>
2016-06-27 14:20:57 +0200

    servers remake

--- src/js/servers.js
+++ src/js/servers.js
@@ -11,57 +11,42 @@ $(document).ready(function() {
     active: false
   }];
 
-  $('.moreinfo' + servers[0].name).hide();
-  $('.moreinfo' + servers[1].name).hide();
+  $('#' + servers[0].name + 'INFO').slideToggle();
+  $('#' + servers[1].name + 'INFO').slideToggle();
+  $('#' + servers[2].name + 'INFO').slideToggle();
+
+  loadJSON(0);
+  loadJSON(1);
 
   function toggleserver(server) {
-    if (servers[server].active == true) {
-      $("#show"+servers[server].name).html('MORE INFO');
-      for (var i = 0; i < servers.length; i++) {
-        if (i != server) {
-          $('#' + servers[i].name).show();
-        }
-      }
-      $('.moreinfo' + servers[server].name).hide();
-      servers[server].active = false;
-    } else {
-      $("#show"+servers[server].name).html('LESS INFO');
-      for (var i = 0; i < servers.length; i++) {
-        if (i != server) {
-          $('#' + servers[i].name).hide();
-        }
-      }
-      $('.moreinfo' + servers[server].name).show();
-      servers[server].active = true;
-    }
+    $("#" + servers[server].name + 'SHOW > i').toggleClass('fa fa-chevron-down');
+    $("#" + servers[server].name + 'SHOW > i').toggleClass('fa fa-chevron-up');
+    $('#' + servers[server].name + 'INFO').slideToggle();
   }
 
-  $('#showSERVER1').click(function() {
+  function loadJSON(server) {
+    var showData = $('#' + servers[server].name + 'TABLE');
+
+    $.getJSON('modlist.json', function(data) {
+      console.log(data);
+      var items = data.items.map(function(mod) {
+        if (mod.server == server) {
+          return '<tr>' + '<th>' + mod.name + '</th>' + '<th>' + mod.version + '</th>' + '<th>' + '<a href="' + mod.url + ' target="_blank"><i class="fa fa-download"></i></a>' + '</th>' + '</tr>';
+        }
+      });
+      showData.empty();
+      if (items.length) {showData.append(items);}
+    });
+
+  }
+
+  $('#SERVER1MAIN').unbind().click(function() {
     toggleserver(0);
-    var liSERVER1 = $('#tableSERVER1');
-    $.getJSON('modlist.json', function(data) {
-      var content_SERVER1 = data.SERVER1.map(function(mod) {
-        return '<tr>' + '<th>' + mod.name + '</th>' + '<th>' + mod.version + '</th>' + '<th>' + '<a href="' + mod.url + ' target="_blank"><i class="fa fa-download"></i></a>' + '</th>' + '</tr>';
-      });
-      liSERVER1.empty();
-      if (content_SERVER1.length) {
-        liSERVER1.append(content_SERVER1);
-      }
-    });
   });
-
-  $('#showSERVER2').click(function() {
+  $('#SERVER2MAIN').click(function() {
     toggleserver(1);
-    var liSERVER2 = $('#tableSERVER2');
-    $.getJSON('modlist.json', function(data) {
-      var content_SERVER2 = data.SERVER2.map(function(mod) {
-        return '<tr>' + '<th>' + mod.name + '</th>' + '<th>' + mod.version + '</th>' + '<th>' + '<a href="' + mod.url + ' target="_blank"><i class="fa fa-download"></i></a>' + '</th>' + '</tr>';
-      });
-      liSERVER2.empty();
-      if (content_SERVER2.length) {
-        liSERVER2.append(content_SERVER2);
-      }
-    });
   });
-
+  $('#SERVER3MAIN').click(function() {
+    toggleserver(2);
+  });
 });
